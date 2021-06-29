c0f8d4fbf84b0fbcfc9ff3616783aeb892644913
Bharath <bt9944@gmail.com>
2021-06-29 10:13:05 +0530

    UI: Samsung watch minute text

--- lib/clock_faces/samsung_watch_face.dart
+++ lib/clock_faces/samsung_watch_face.dart
@@ -5,15 +5,21 @@ import 'dart:ui';
 import 'package:flutter/material.dart';
 
 class SamsungWatchFace extends CustomPainter {
-  static const int _numberOfMintuesPointer = 60;
-  static const double _angleBetweenMinutePointers = (2 * math.pi) / _numberOfMintuesPointer;
+  static const int _numberOfMintues = 60;
+  static const double _angleBetweenMinutePointers = (2 * math.pi) / _numberOfMintues;
 
   final double spaceBeyondMinuteLine;
+  final double minuteLineLength;
+  final double minuteLineDivBy5Length;
+  final double minuteLineDivBy15Length;
   late final double _baseCircleRadius;
   late final double _canvasSide;
   late final double _minuteLineExtremePointRadius;
   SamsungWatchFace({
     this.spaceBeyondMinuteLine = 4,
+    this.minuteLineLength = 4,
+    this.minuteLineDivBy5Length = 7,
+    this.minuteLineDivBy15Length = 10,
   });
 
   @override
@@ -23,10 +29,10 @@ class SamsungWatchFace extends CustomPainter {
     _baseCircleRadius = _canvasSide / 2;
     _minuteLineExtremePointRadius = _baseCircleRadius - spaceBeyondMinuteLine;
     _drawBaseCircle(canvas);
-    _drawMinutesPointer(canvas);
+    _drawMinutesLine(canvas);
     _drawCenterCircle(canvas);
     // _drawHoursHand(canvas, _side);
-    // _drawMinutesPointer(canvas, _side);
+    _drawMinutesPointer(canvas);
   }
 
   void _drawBaseCircle(Canvas canvas) {
@@ -34,88 +40,74 @@ class SamsungWatchFace extends CustomPainter {
       ..shader = ui.Gradient.linear(
         Offset(_canvasSide / 2, _canvasSide),
         Offset(_canvasSide / 2, 0),
-        [Colors.cyan[400]!, Colors.cyan[100]!],
+        [Colors.cyan[500]!, Colors.cyan[700]!],
       )
       ..style = PaintingStyle.fill;
     canvas.drawCircle(Offset(_canvasSide / 2, _canvasSide / 2), 100, _paint);
   }
 
-  void _drawMinutesPointer(Canvas canvas) {
+  void _drawMinutesLine(Canvas canvas) {
     canvas.save();
     canvas.translate(_baseCircleRadius, _baseCircleRadius);
-    // canvas.scale(1, -1);
     final Paint _paint = Paint()
       ..color = Colors.white
       ..strokeWidth = 2
       ..strokeCap = StrokeCap.round
       ..style = PaintingStyle.stroke;
-    for (int i = 0; i <= _numberOfMintuesPointer; i++) {
-      final Offset _offsetOne =
-          _getExtremePointOfTheMinuteLine(minute: i, minuteLineRadius: _minuteLineExtremePointRadius);
-      final Offset _offsetTwo =
-          _getInnerPointOfTheMinuteLine(minute: i, minuteLineRadius: _minuteLineExtremePointRadius);
-      canvas.drawLine(_offsetOne, _offsetTwo, _paint);
-      // canvas.drawPoints(PointMode.points, [_offsetTwo], _paint);
-      if (i % 5 == 0) {}
+    for (int minute = 0; minute <= _numberOfMintues; minute++) {
+      final double _theta = _angleBetweenMinutePointers * minute;
+      final double _radiusOfInnerPoint = _getInnerPointRadiusOfMinuteLine(minute);
+
+      final Offset _extremePoint = _getPointWithRadiusAndTheta(radius: _minuteLineExtremePointRadius, theta: _theta);
+      final Offset _innerPoint = _getPointWithRadiusAndTheta(radius: _radiusOfInnerPoint, theta: _theta);
+      canvas.drawLine(_extremePoint, _innerPoint, _paint);
+      if (minute % 5 == 0) {
+        _drawMinuteText(minute: minute, canvas: canvas, theta: _theta);
+      }
     }
     canvas.restore();
   }
 
-  Offset _getExtremePointOfTheMinuteLine({required double minuteLineRadius, required int minute}) {
-    final double _theta = _angleBetweenMinutePointers * minute;
-    return Offset(minuteLineRadius * math.cos(_theta), minuteLineRadius * math.sin(_theta));
+  Offset _getPointWithRadiusAndTheta({required double radius, required double theta}) {
+    return Offset(radius * math.cos(theta), radius * math.sin(theta));
   }
 
-  Offset _getInnerPointOfTheMinuteLine({required double minuteLineRadius, required int minute}) {
-    final double _minuteLineLength = 4;
-    final double _minuteLineDivBy5Len = 7;
-    final double _minuteLineDivBy15Len = 10;
-    final double _theta = _angleBetweenMinutePointers * minute;
-    return Offset(
-        (minuteLineRadius -
-                ((minute % 15 == 0)
-                    ? _minuteLineDivBy15Len
-                    : (minute % 5 == 0)
-                        ? _minuteLineDivBy5Len
-                        : _minuteLineLength)) *
-            math.cos(_theta),
-        (minuteLineRadius -
-                ((minute % 15 == 0)
-                    ? _minuteLineDivBy15Len
-                    : (minute % 5 == 0)
-                        ? _minuteLineDivBy5Len
-                        : _minuteLineLength)) *
-            math.sin(_theta));
+  double _getInnerPointRadiusOfMinuteLine(int minute) {
+    return _minuteLineExtremePointRadius -
+        ((minute % 15 == 0)
+            ? minuteLineDivBy15Length
+            : (minute % 5 == 0)
+                ? minuteLineDivBy5Length
+                : minuteLineLength);
   }
 
-  void _drawMinuteText({required int minute, required Canvas canvas}) {
-    //((minute + 15) % 60)
-    TextSpan span = new TextSpan(style: new TextStyle(color: Colors.black, fontSize: 12.0), text: minute.toString());
+  void _drawMinuteText({required Canvas canvas, required int minute, required double theta}) {
+    final double fontSize = 12.0;
+    TextSpan span = new TextSpan(
+        style: new TextStyle(color: Colors.white, fontSize: fontSize), text: ((minute + 15) % 60).toString());
     TextPainter tp = new TextPainter(
       text: span,
       textAlign: TextAlign.center,
       textDirection: TextDirection.ltr,
     );
-    tp.layout(minWidth: 16);
-    tp.size;
-
-    final Offset _offsetTwo =
-        _getInnerPointOfTheMinuteLine(minute: minute, minuteLineRadius: _minuteLineExtremePointRadius);
-
-    tp.paint(canvas, Offset(_offsetTwo.dx - 8, _offsetTwo.dy));
+    tp.layout();
+    final Offset _pointToPositionText =
+        _getPointWithRadiusAndTheta(radius: _getInnerPointRadiusOfMinuteLine(minute) - 12, theta: theta);
+    tp.paint(canvas, Offset(_pointToPositionText.dx - tp.width / 2, _pointToPositionText.dy - tp.height / 2));
   }
 
-  // void _drawMinutesPointer(Canvas canvas, double side) {
-  //   final Paint _paint = Paint()
-  //     ..style = PaintingStyle.fill
-  //     ..strokeWidth = 2
-  //     ..strokeCap = ui.StrokeCap.round
-  //     ..color = Colors.white;
-  //
-  //   canvas.translate(side / 2, side / 2);
-  //   canvas.scale(1, -1);
-  //   canvas.drawLine(Offset(0, 0), Offset(100, 50), _paint);
-  // }
+  void _drawMinutesPointer(Canvas canvas) {
+    final Paint _paint = Paint()
+      ..style = PaintingStyle.fill
+      ..strokeWidth = 2
+      ..strokeCap = ui.StrokeCap.round
+      ..color = Colors.white;
+
+    // final double _minutePointerLength = 10;
+    //
+    // canvas.drawLine(Offset(_canvasSide / 2, _canvasSide / 2),
+    //     _getPointWithRadiusAndTheta(radius: _minutePointerLength, theta: 50), _paint);
+  }
 
   void _drawCenterCircle(Canvas canvas) {
     final Paint _paint = Paint()
